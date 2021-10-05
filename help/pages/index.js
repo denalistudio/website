619af381e83f7a64fcece82efe34942569f72c93
David Brzý <davidbrzy@studiodenali.cz>
2021-10-05 16:48:15 +0200

    help update

--- help/pages/index.js
+++ help/pages/index.js
@@ -1,7 +1,9 @@
+import fs from 'fs'
+import path from 'path'
 import Head from 'next/head'
 
-export default function Home() {
-    return(
+export default function Home({ posts }) {
+    return (
         <div>
             <Head>
                 <title>Dev Blog</title>
@@ -9,4 +11,30 @@ export default function Home() {
             <h2>Hello</h2>
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const files = fs.readdirSync(path.join('posts'))
+
+    // Get slug and frontmatter from posts
+    const posts = files.map(filename => {
+        // Create slug
+        const slug = filename.replace('.md', '')
+        // Get frontmatter
+        const markdownWithMeta = fs.readFileSync(
+            path.join('posts', filename),
+            'utf-8'
+        )
+        console.log(markdownWithMeta)
+        return {
+            slug,
+        }
+
+    })
+
+    return {
+        props: {
+            posts: 'The Posts'
+        }
+    }
 }
---
+++ help/posts/test.md
@@ -0,0 +1,12 @@
+---
+title: 'Test Post'
+date: '5. 10. 2021'
+excerpt: 'This is excerpt'
+cover_image: '/images/posts/img1.jpg'
+---
+## Heading Two
+
+- Item 1
+- Item 2
+
+Hello World
