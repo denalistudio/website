2977396880583805231fd8863cdfe9ec9e4df695
David Brzý <davidbrzy@studiodenali.cz>
2021-10-05 23:26:14 +0200

    update

--- help/pages/index.js
+++ help/pages/index.js
@@ -2,17 +2,17 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import Head from 'next/head'
+import Post from '../components/Post'
 
 export default function Home({ posts }) {
-    console.log(posts)
     return (
         <div>
             <Head>
                 <title>Dev Blog</title>
             </Head>
-            <div className="posts">
+            <div className='posts'>
                 {posts.map((post, index) => (
-                    <h3>{post.frontmatter.title}</h3>
+                    <Post post={post} />
                 ))}
             </div>
         </div>
@@ -21,25 +21,23 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
     const files = fs.readdirSync(path.join('posts'))
-
     // Get slug and frontmatter from posts
-    const posts = files.map(filename => {
+    const posts = files.map((filename) => {
         const slug = filename.replace('.md', '')
         const markdownWithMeta = fs.readFileSync(
             path.join('posts', filename),
             'utf-8'
         )
-        const {data: frontmatter} = matter(markdownWithMeta)
+        const { data: frontmatter } = matter(markdownWithMeta)
         return {
             slug,
             frontmatter
         }
 
     })
-
     return {
         props: {
-            posts,
+            posts
         }
     }
 }
