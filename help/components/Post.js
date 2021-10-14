cebf63e4ebaafa04983662d72e16298576ce3834
David Brzý <davidbrzy@studiodenali.cz>
2021-10-14 18:37:49 +0200

    enter

--- help/components/Post.js
+++ help/components/Post.js
@@ -3,14 +3,13 @@ import Link from 'next/Link'
 export default function Post({post}) {
     return (
         <div class='card'>
-            <img src={post.frontmatter.cover_image} />
-            <div className='post-date'>Posted on {post.frontmatter.date}</div>
-            <h3>{post.frontmatter.title}</h3>
             <p>{post.frontmatter.category}</p>
+            <h3>{post.frontmatter.title}</h3>
             <p>{post.frontmatter.description}</p>
-            <Link href={`/blog/${post.slug}`}>
-                <a className='btn'>Read more</a>
-            </Link>
+            <img src={post.frontmatter.cover_image} /> 
         </div>
     )
 }
+
+// <div className='post-date'>Posted on {post.frontmatter.date}</div>
+// <Link href={`/blog/${post.slug}`}><a className='btn'>Read more</a></Link>
