d6e1b55d31edb06cb7cd737acaf949ccdafb4e62
David Brzý <davidbrzy@studiodenali.cz>
2021-10-14 21:14:13 +0200

    enter

--- help-dev/next.config.js
+++ help-dev/next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
     assetPrefix: './',
-    async rewrites() {
-        return [
-          {
-            source: '/blog/',
-            destination: './blog/',
-          },
-        ]
-      },
     trailingSlash: true,
     reactStrictMode: true
 }
