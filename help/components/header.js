21c914d9c7e32df54f99313abe6a84c37fc81e1a
David Brzý <davidbrzy@studiodenali.cz>
2021-10-14 17:49:31 +0200

    enter

--- help/components/header.js
+++ help/components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <header>
             <div className='container'>
                 <Link href='/' passHref>
-                    <h2>Dev Blog</h2>
+                    <h2>Znalostní báze | Studio Denali</h2>
                 </Link>
             </div>
         </header>
