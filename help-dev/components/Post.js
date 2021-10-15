99e8c257543ec6c56696c33e317e3e0e6dab9c75
David Brzý <davidbrzy@studiodenali.cz>
2021-10-15 07:36:36 +0200

    enter

--- help-dev/components/Post.js
+++ help-dev/components/Post.js
@@ -3,9 +3,9 @@ import Link from 'next/Link'
 export default function Post({ post }) {
     return (
         <div class='card'>
-            <Link href={`https://www.studiodenali.cz/help/blog/${post.slug}`}>
+            <Link href={`./blog/${post.slug}`}>
                 <a>
-                    <img src={`./${post.frontmatter.cover_image}`} />
+                    <img src={`.${post.frontmatter.cover_image}`} />
                     <h4>{post.frontmatter.category}</h4>
                     <h3>{post.frontmatter.title}</h3>
                     <p>{post.frontmatter.description}</p>
