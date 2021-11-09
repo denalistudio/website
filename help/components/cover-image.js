27b7f41e8d025c86a89c7c0a29711ccf4ef16146
David Brzý <davidbrzy@studiodenali.cz>
2021-11-09 17:48:59 +0100

    blog update

--- help/components/cover-image.js
+++ help/components/cover-image.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 
-export default function CoverImage({ src, height, width }) {
+export default function CoverImage({ src, width, height }) {
     const image = (
         <Image
             src={src}
+            className="post-image"
+            layout="fill"
             width={width}
             height={height}
         />
