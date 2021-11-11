7493b8451f715d82c22efd165f84cbd908d05aa9
David Brzý <davidbrzy@studiodenali.cz>
2021-11-11 19:36:42 +0100

    blog update

--- help/components/cover-image.js
+++ help/components/cover-image.js
@@ -1,11 +1,13 @@
+import Image from 'next/image'
+
 export default function CoverImage({ src, width, height }) {
     return (
-        <img
+        <Image
             src={src}
             className="post-image"
-            layout="fill"
-            width={width}
-            height={height}
+            layout="responsive"
+            width = {width}
+            height = {height}
         />
     )
 }
