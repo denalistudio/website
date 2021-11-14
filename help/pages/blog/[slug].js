3c86215223f7633f082c0df72aa97af02604656e
David Brzý <davidbrzy@studiodenali.cz>
2021-11-14 14:40:32 +0100

    blog update

--- help/pages/blog/[slug].js
+++ help/pages/blog/[slug].js
@@ -34,12 +34,12 @@ export async function getStaticPaths() {
     const files = fs.readdirSync(path.join('posts'))
     const paths = files.map(filename => ({
         params: {
-            slug: filename.replace('.md', '')
-        }
+            slug: filename.replace('.md', ''),
+        },
     }))
     return {
         paths,
-        fallback: false
+        fallback: false,
     }
 }
 
