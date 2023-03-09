df7f3ac3b3452971000ed8ea35a31f6bc544a264
David Brzý <brzy@studiodenali.cz>
2023-03-09 19:10:43 +0100

    update docs/index.html, style.min.css, udime_doma.mov and style.scss

---
+++ docs/script.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    $("video").each(function() {
+        const video = $(this);
+        if (video.visible(true)) {
+            video[0].play();
+        } else {
+            video.prop("currentTime", 0);
+            video[0].pause();
+        };
+    });
+});
