6e715e0855568c280bf35a4ea8cfde061b3fc183
David Brzý <brzy@studiodenali.cz>
2023-06-19 16:48:46 +0200

    delete 16 files and update 2 files

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,2 @@
+// Write current year into the copyright claim
 document.getElementById("copyright_year").innerHTML = new Date().getFullYear();
-
-document.getElementById("kontakt-anchor").onclick = function () {
-    document.getElementById("kontakt").scrollIntoView({ behavior: "smooth" });
-}
-
-$(window).scroll(function () {
-    $("video").each(function () {
-        const video = $(this);
-        if (video.visible(true)) {
-            video[0].play();
-        } else {
-            video.prop("currentTime", 0);
-            video[0].pause();
-        };
-    });
-});
