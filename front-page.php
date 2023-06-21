5503032ff97f043f4fd76d90dfbea29a6d008056
David Brzý <brzy@studiodenali.cz>
2023-06-21 16:04:07 +0200

    update 7 files

--- front-page.php
+++ front-page.php
@@ -15,14 +15,14 @@ get_header();
 ?>
 <div class="container">
     <h1 class="page_title">We help companies thrive <br>in our <span class="gradient">ever-changing world</span></h1>
-    <figure class="hero">
+    <figure class="hero_image">
         <picture>
             <source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/hero.webp" type="image/webp">
             <source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/hero.png" type="image/png">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero.png" alt="">
         </picture>
     </figure>
-    <p>We are a multidisiplinary digital agency that helps companies succeed by crafting brands that last, creating
+    <p class="hero_text">We are a multidisiplinary digital agency that helps companies succeed by crafting brands that last, creating
         experiences that impress and stories that inspire.</p>
 </div>
 <div class="contact" data-anchor="contact">
