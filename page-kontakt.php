f76aa5bbe7689c0513f8d23d96a7c612169f4e92
David Brzý <brzy@studiodenali.cz>
2023-06-29 11:51:19 +0200

    update page-kontakt.min.css, contact-form.js, page-kontakt.scss and page-kontakt.php

--- page-kontakt.php
+++ page-kontakt.php
@@ -15,9 +15,9 @@ if (have_posts()) :
             <h1>Dobrý den, řekněte nám něco o sobě</h1>
             <div class="contact_form">
                 <p>Jmenuji se <input type="text" name="first_name" id="first_name" placeholder="jméno"><input type="text" name="last_name" id="last_name" placeholder="příjmení"></p>
-                <p>Můžete mě zastihnout na e-mailové adrese <input type="email" name="email" id="email" placeholder="e-mailová adresa"></p>
+                <p>Můžete mě zastihnout na e-mailové adrese <input type="email" name="email" id="email" placeholder="vaše e-mailová adresa"></p>
                 <div class="message">
-                    <p>Zde je moje zpráva pro vás:</p>
+                    <p>Co byste chtěli, abychom pro Vás vytvořili?</p>
                     <div id="message_wrapper">
                         <textarea name="message" id="message" cols="50" rows="10"></textarea>
                     </div>
