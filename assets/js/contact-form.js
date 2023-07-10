c51fe8167dd9b945ea7598d2858ef8253989fbe0
David Brzý <brzy@studiodenali.cz>
2023-07-10 09:33:22 +0200

    update 9 files

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -22,17 +22,53 @@ function contactFormMessage() {
 };
 contactFormMessage();
 
-function Make() {
-    const firstName = document.getElementById("first_name").value;
-    const lastName = document.getElementById("last_name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+function sendForm() {
+    const selectedServices = document.querySelectorAll('.service[data-selected="true"]');
+    const firstName = document.getElementById("first_name");
+    const lastName = document.getElementById("last_name");
+    const company = document.getElementById("company");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
 
-    if (firstName !== "" & lastName !== "" & email !== "" & message !== "") {
+    let formValid = false;
+
+    document.querySelectorAll("#first_name, #last_name, #company, #email, #message").forEach((input) => {
+        const value = input.value;
+        if (value === "") {
+            input.classList.add("error");
+        } else {
+            input.classList.add("success");
+        };
+    });
+
+    if (selectedServices.length > 0 && firstName.value !== "" && lastName.value !== "" && company.value !== "" && email.value !== "" && message.value !== "") {
+        formValid = true;
+    };
+
+    if (formValid) {
+        console.log("Form is valid");
+
+        document.querySelectorAll("#first_name, #last_name, #company, #email, #message").forEach((input) => {
+            input.classList.remove("error");
+            input.classList.add("success");
+
+            setTimeout(() => {
+                firstName.value = "";
+                lastName.value = "";
+                company.value = "";
+                email.value = "";
+                message.value = "";
+                input.classList.remove("success");
+            }, 100);
+        });
+
+        window.location.href = "http://localhost/denali/odeslany-formular";
+    };
+    
+    /*if (firstName !== "" & lastName !== "" & email !== "" & message !== "") {
         const webhookClickUp = `https://hook.eu1.make.com/5qy32v9vdu3l3l493jk3xh62nibx3gx8?firstName=${firstName}&lastName=${lastName}&email=${email}&message=${message}`;
         const webhookEcomail = `https://hook.eu1.make.com/ksivdbgtu20m6m5xt1lnkfoehjd459cu?firstName=${firstName}&lastName=${lastName}&email=${email}&message=${message}`;
         fetch(webhookClickUp);
         fetch(webhookEcomail);
-        console.log("Data fetched successfully");
-    };
+    };*/
 };
--- page-kontakt.php
+++ page-kontakt.php
@@ -26,9 +26,9 @@ if (have_posts()) :
                     </div>
                 </section>
                 <section>
-                    <p class="margin">Jmenuji se <input type="text" name="first_name" id="first_name" placeholder="jméno" class="ml-1 mr-05" required><input type="text" name="last_name" id="last_name" placeholder="příjmení" class="ml-05 mr-05" required></p>
-                    <p class="margin">a zastupuji <input type="text" name="company" id="company" placeholder="název firmy nebo jméno OSVČ" class="ml-1" required></p>
-                    <p class="margin">Můžete mě zastihnout na e-mailové adrese <input type="email" name="email" id="email" placeholder="vaše e-mailová adresa" class="ml-1" required></p>
+                    <p class="margin">Jmenuji se <input type="text" name="first_name" id="first_name" placeholder="jméno" class="ml-1 mr-05"><input type="text" name="last_name" id="last_name" placeholder="příjmení" class="ml-05 mr-05"></p>
+                    <p class="margin">a zastupuji <input type="text" name="company" id="company" placeholder="název firmy nebo obchodní jméno" class="ml-1"></p>
+                    <p class="margin">Můžete mě zastihnout na e-mailové adrese <input type="email" name="email" id="email" placeholder="vaše e-mailová adresa" class="ml-1"></p>
                 </section>
                 <section>
                     <p>Popište nám, co pro Vás máme vytvořit:</p>
@@ -36,7 +36,11 @@ if (have_posts()) :
                         <textarea name="message" id="message" cols="50" rows="5"></textarea>
                     </div>
                 </section>
-                <button class="btn-black" onclick="Make()">Odeslat</button>
+                <section>
+                    <input type="checkbox" name="consent" id="consent">
+                    <label for="checkbox">Souhlasím se zasíláním organizačních a informačních e-mailů ohledně Vaší poptávky.</label>
+                </section>
+                <button id="contact_form__btn" class="btn-black" onclick="sendForm();">Odeslat</button>
             </div>
         </main>
     </div>
